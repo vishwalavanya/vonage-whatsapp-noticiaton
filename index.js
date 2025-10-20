019668a64fc2b7df6fc67415f82e8eb920fe3706
vishwa <123007235+vishwalavanya@users.noreply.github.com>
2025-10-20 22:17:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // ------------------ CONFIG ------------------ //
